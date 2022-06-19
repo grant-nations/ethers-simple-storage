36fcaf2266a69487e00ec0e8040f2ac8edc4bf04
Grant Nations <nationsgrant@gmail.com>
2022-06-19 12:11:29 -0600

    first commit

---
+++ deploy.js
@@ -0,0 +1,11 @@
+// main script for deploying SimpleStorage.sol
+async function main() {
+  console.log("Hello, world.");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
