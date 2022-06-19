705321d03a271142ec2055ab5de028c5934531af
Grant Nations <nationsgrant@gmail.com>
2022-06-19 12:58:56 -0600

    write code to deploy contract to ganache instance

--- deploy.js
+++ deploy.js
@@ -1,6 +1,28 @@
 // main script for deploying SimpleStorage.sol
+require("dotenv").config();
+const fs = require("fs");
+const ethers = require("ethers");
+
 async function main() {
-  console.log("Hello, world.");
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.GANACHE_RPC_SERVER_ADDRESS
+  );
+  const wallet = new ethers.Wallet(
+    process.env.GANACHE_ACCT_0_PRIVATE_KEY,
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+
+  // a Contract factory is an object you can use to deploy contracts
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying contract...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
